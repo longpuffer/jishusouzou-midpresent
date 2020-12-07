af863d78ab2fe707ba164b93b71399915f0d3392
lopngpuffer <70842199+lopngpuffer@users.noreply.github.com>
2020-12-07 12:11:03 +0900

    a

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 250) {
-        $('#menu-bar').addClass('fixed');
-      } else {
-        $('#menu-bar').removeClass('fixed');
-      }
-    });
-  });

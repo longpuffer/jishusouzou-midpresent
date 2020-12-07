bb4d4ee7d43c5b33bb06d9c7a99944cd6ff36b91
lopngpuffer <70842199+lopngpuffer@users.noreply.github.com>
2020-12-07 17:27:55 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
-$(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 250) {
-        $('#menu-bar').addClass('fixed');
-      } else {
-        $('#menu-bar').removeClass('fixed');
-      }
-    });
-  });
+
